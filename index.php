5b88f5c5b6d652d026b4951ebe2156d946512da5
ftellez <fernando.saisd@gmail.com>
2016-11-17 20:33:26 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$conn = mysqli_connect('localhost', 'u278220770_sens', 'D33qsoG96z', 'u278220770_proy'); //The Blank string is the password
+
+$query = "SELECT data FROM Prueba"; //You don't need a ; like you do in SQL
+$result= mysqli_query($conn,$query);
+
+echo "<table>"; // start a table tag in the HTML
+
+while($row = mysqli_fetch_assoc($result)){   //Creates a loop to loop through results
+echo "<tr><td>" . $row['data'] . "</td></tr>" ;  //$row['index'] the index here is a field name
+}
+
+echo "</table>"; //Close the table in HTML
+
+?>
